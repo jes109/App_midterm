356698f714cc1eeece6e1d5b8d86ebf4afdac941
jess109 <comirai1009@gmail.com>
2024-04-14 20:10:24 +0800

    更新SaveScreen

--- src/screens/SaveScreen.js
+++ src/screens/SaveScreen.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import SegmentedControlTab from "react-native-segmented-control-tab"
 import { Box,Center,Pressable,Text ,FlatList} from "@gluestack-ui/themed";
 import { StyleSheet } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation,useTheme } from '@react-navigation/native';
 
 import EventList from "../components/EventList"
 import EventItem from "../components/EventItem";
@@ -11,17 +11,37 @@ import EventItem from "../components/EventItem";
 import Events from "../json/Events.json"
 
 export default SaveScreen = () => {
-    const [segmentIndex,setSegmentIndex] = useState(0);
+    const {colors} =useTheme();
 
-    const SegmentContent = () =>{
-        return(
-            !segmentIndex? <Join/>:<Mark/>
-        );
-    }
+    const [segmentIndex,setSegmentIndex] = useState(0);
+    const SegmentContent = () =>!segmentIndex? <Join/>:<Mark/>
+
     return(
-        <Box>
+        <Box bg={colors.surface}>
             <SegmentedControlTab 
             values={["已參加","收藏"]}
+            tabStyle={{
+                marginTop:20,
+                marginBottom:20,
+                borderWidth:2,
+                backgroundColor:"transparent",
+                borderColor:colors.primary800
+            }}
+            firstTabStyle={{
+                marginLeft:20
+            }}
+            lastTabStyle={{
+                marginRight:20
+            }}
+            tabTextStyle={{
+                color:colors.primary800
+            }}
+            activeTabStyle={{
+                backgroundColor:colors.lightsurface
+            }}
+            activeTabTextStyle={{
+                color:colors.primary800
+            }}
             selectedIndex={segmentIndex}
             onTabPress={(index)=>setSegmentIndex(index)}
             />
@@ -34,22 +54,28 @@ const Mark=()=>{
     const renderItem=({item})=>item.mark?<EventItem event={item}/>:null;
 
     return(
-        <FlatList
-        data={Events}
-        renderItem={renderItem}
-        keyExtractor={(item,index)=>index+item}
-        />
+        <Center >
+            <FlatList
+            data={Events}
+            renderItem={renderItem}
+            keyExtractor={(item,index)=>index+item}
+            showsVerticalScrollIndicator={false}
+            />
+        </Center>
     )
 }
 const Join=()=>{
     const renderItem=({item})=>item.join?<EventItem event={item}/>:null;
 
     return(
-        <FlatList
-        data={Events}
-        renderItem={renderItem}
-        keyExtractor={(item,index)=>index+item}
-        />
+        <Center>
+            <FlatList
+            data={Events}
+            renderItem={renderItem}
+            keyExtractor={(item,index)=>index+item}
+            showsVerticalScrollIndicator={false}
+            />
+        </Center>
     )
 }
 
