beedcbda36326aa1c2f8748a656f04f94a6f70f7
jess109 <comirai1009@gmail.com>
2024-04-15 20:40:40 +0800

    上傳圖

--- src/screens/EventDetailScreen.js
+++ src/screens/EventDetailScreen.js
@@ -8,43 +8,40 @@ export default EventDetailScreen = ({route}) => {
     const {title,location,img,date,limit,number,trait,description,join}=route.params;
     const [num,setNum]=useState(number);
     const [hasJoin,setHasJoin]=useState(join);
-    const opacity=!hasJoin?1:0.6;
+    const opacity=!hasJoin?1:0.5;
 
     return(
         <Box flex={1}>
-            <ScrollView>
-                <Box bg={colors.card} pt={20} px={16} >
+            <ScrollView bg={colors.card}>
+                <Box py={20} px={16} >
                     <Text size="2xl" bold="true" color={colors.primary800} mb={8}>{title}</Text>
                     <Image source={img} alt="pic" w="auto" h={200} mb={20} rounded="$2xl"/>
-                    <HStack justifyContent="space-between" borderBottomWidth={1} borderBottomColor={colors.primary200} pb={16}>
+                    <VStack justifyContent="space-between" borderBottomWidth={1} borderBottomColor={colors.primary200} pb={16}>
                         <VStack gap={8}>
                             <Text size="md" bold="true" color={colors.primary500}>日期:{date}</Text>
                             <Text size="md" bold="true" color={colors.primary500}>地點:{location}</Text>
                             <Text size="md" bold="true" color={colors.primary500}>人數:{limit}</Text>
                             <Text size="md" bold="true" color={colors.primary500}>建議條件:{trait}</Text>
                         </VStack>
-                        <Text mt={96} px={12} py={4} size="md" rounded="$2xl" bold="true" color={colors.primary500}  borderColor={colors.primary500} borderWidth={1} >目前參與人數:{num}</Text>
-                    </HStack>
-                    <Text color="black" mt={20} size="md" bold="true" >{description}</Text>
-                </Box>
-            </ScrollView>
-            <TouchableOpacity onPress={()=>{setNum(num+1);setHasJoin(true)}} activeOpacity={0.6} disabled={hasJoin}>
-                <HStack rounded="$full" py={12} style={styles.fab} bg={colors.primaryContainer} flex={1} justifyContent="center" opacity={opacity}>
+                        <Text mt={12} alignSelf="flex-end" px={12} py={4} size="md" rounded="$2xl" bold="true" color={colors.primary500}  borderColor={colors.primary500} borderWidth={1} >目前參與人數:{num}</Text>
+                    </VStack>
+                    <Text color="black" mt={20} size="md" >{description}</Text>
+                    <TouchableOpacity onPress={()=>{setNum(num+1);setHasJoin(true)}} activeOpacity={0.6} disabled={hasJoin}>
+                    <HStack rounded="$full" py={12} style={styles.fab} bg={colors.primaryContainer} flex={1} justifyContent="center" opacity={opacity}>
                     <Icon as={AddIcon} color={colors.primary800}/>
                     <Text color={colors.primary800} bold="true">參加</Text>
                 </HStack>
-            </TouchableOpacity>
+                </TouchableOpacity>
+                </Box>
+            </ScrollView>
         </Box>
            
     )
 }
 const styles=StyleSheet.create({
     fab:{
+        alignSelf:"flex-end",
         width:80,
-        position:"absolute",
-        right:12,
-        bottom:20,
-        zIndex:999,
         shadowColor: "#000",
         shadowOffset: {
 	        width: 2,
