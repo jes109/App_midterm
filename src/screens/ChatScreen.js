346b40141f4d2335ceff5f04ad70b4671dbbd60a
jess109 <comirai1009@gmail.com>
2024-04-16 00:13:18 +0800

    img update

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTheme } from "@react-navigation/native";
-import { View, Box, Image, Pressable, Text, Center } from "@gluestack-ui/themed";
+import { View, Box, Image, Pressable, Text, Center, ScrollView } from "@gluestack-ui/themed";
 import { StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
@@ -11,10 +11,11 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default ChatScreen = ({navigation}) => {
     const {colors} =useTheme();
     return(
-            <Box bg={colors.card} flex={1}>
+            <ScrollView bg={colors.card} flex={1}>
                 <Center>
                     <ChatList/>
+                    <Image rounded="$xl" source={"https://github.com/jes109/App_midterm/blob/master/src/img/cover.png?raw=true"} alt="d" h={60} w={60}/>
                 </Center>
-            </Box>
+            </ScrollView>
     )
 }
